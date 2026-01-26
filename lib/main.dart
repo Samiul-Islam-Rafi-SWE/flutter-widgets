9312f624b9620752b06d40ab77bbb62fff3de091
samiul77RR <157511343+samiul77RR@users.noreply.github.com>
2026-01-27 01:34:07 +0600

    OverFlow For Display

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class myApp extends StatelessWidget {
     return MaterialApp(
       title: 'Essential widgets',
       debugShowCheckedModeBanner: false,
-      home: X(),
+      home: Home(),
     );
   }
 }
@@ -27,6 +27,11 @@ final List<String> studentList = [
   'kamrul',
   'Rofiq',
   'kamal',
+  'Simul',
+  'Daddodil',
+  'Fokir',
+  'Salam',
+  'Rabbi',
 ];
 
 class Home extends StatelessWidget {
@@ -158,108 +163,45 @@ class Home extends StatelessWidget {
       //   },
       // ),
       //
-      body: SingleChildScrollView(
-        child: ListView.builder(
-          // physics: NeverScrollableScrollPhysics(),
-          itemCount: studentList.length,
-          itemBuilder: (context, index) {
-            return Row(
-              children: [
-                Text("${index + 1}. ", style: TextStyle(color: Colors.black87)),
-                Text(studentList[index]),
-              ],
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class X extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        top: true,
-        bottom: true,
-        child: SingleChildScrollView(
-          child: Column(
-            // spacing: 10,
-            children: [
-              // appbar part
-              Container(
-                color: Colors.blueAccent,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 12,
-                    horizontal: 4,
-                  ),
-                  child: Center(child: Text("Home")),
-                ),
-              ),
-
-              // body
-              Container(
-                height: 200,
-                width: double.infinity,
-                color: Colors.blueGrey,
-              ),
-              Container(
-                height: 200,
-                width: double.infinity,
-                color: Colors.brown,
-              ),
-
-              ListView.builder(
-                physics: NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: studentList.length,
-                itemBuilder: (BuildContext studentContext, int index) {
-                  return Column(
-                    children: [
-                      SizedBox(height: 10),
-                      Container(
-                        color: Colors.lightBlueAccent,
-                        child: Row(
-                          children: [
-                            Text(
-                              "${index + 1}. ",
-                              style: TextStyle(color: Colors.black87),
-                            ),
-                            Text(studentList[index]),
-                          ],
-                        ),
-                      ),
-                    ],
-                  );
-                },
-              ),
-
-              // ...List.generate(studentList.length, (index) {
-              //   return Row(
-              //     children: [
-              //       Text(
-              //         "${index + 1}. ",
-              //         style: TextStyle(color: Colors.black87),
-              //       ),
-              //       Text(studentList[index]),
-              //     ],
-              //   );
-              // }),
-              Container(
-                height: 200,
-                width: double.infinity,
-                color: Colors.brown,
-              ),
-              Container(
-                height: 200,
-                width: double.infinity,
-                color: Colors.blueGrey,
-              ),
-            ],
-          ),
-        ),
+      body: Column(
+        children: [
+          Text('Daffodil International University'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'),
+          Text('dAFFOSDDKNDKDNCKND'), //Overflow For the Text
+        ],
       ),
     );
   }
